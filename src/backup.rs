2568c3a94335a5d7e3dafad8ae361fb5626ba9c6
mousecrusher2 <95418154+mousecrusher2@users.noreply.github.com>
2024-04-17 00:49:26 +0900

    ひととおり実装

---
+++ src/backup.rs
@@ -0,0 +1,102 @@
+use chrono::{DateTime, Local};
+use regex::Regex;
+use std::path::PathBuf;
+use tokio::fs;
+use tokio_stream::{wrappers::ReadDirStream, StreamExt};
+
+pub struct Backuper {
+    backup_folder: PathBuf,
+    origin_folder: PathBuf,
+    backup_count: usize,
+}
+
+impl Backuper {
+    pub(super) fn new(backup_folder: PathBuf, origin_folder: PathBuf, backup_count: usize) -> Self {
+        if !backup_folder.is_dir() {
+            panic!("Backup folder must be a directory");
+        }
+        if !origin_folder.is_dir() {
+            panic!("Origin folder must be a directory");
+        }
+        Self {
+            backup_folder,
+            origin_folder,
+            backup_count,
+        }
+    }
+
+    fn parse_backup_files(&self, file_paths: String) -> Vec<(String, u64)> {
+        thread_local! {
+            static BACKUP_FILE_RE: Regex = Regex::new(r" (?<path>\S+):(?<size>[1-9][0-9]*)").unwrap();
+        }
+        BACKUP_FILE_RE.with(|re| {
+            re.captures_iter(&file_paths)
+                .map(|cap| {
+                    let (_, [path, size]) = cap.extract();
+                    (
+                        path.to_string(),
+                        size.parse().expect("Failed to parse size"),
+                    )
+                })
+                .collect()
+        })
+    }
+
+    pub(super) async fn backup(&self, file_paths: String) {
+        let world_name = file_paths.split('/').next().unwrap();
+        let origin_world_folder = self.origin_folder.join(world_name);
+        let backup_world_folder = self.backup_folder.join(world_name);
+        let mut t = ReadDirStream::new(fs::read_dir(origin_world_folder).await.unwrap());
+        let mut v = Vec::new();
+        while let Some(entry) = t.next().await {
+            let entry = entry.unwrap();
+            if !entry.file_type().await.unwrap().is_dir() {
+                continue;
+            }
+            if let Ok(d) =
+                DateTime::parse_from_str(entry.file_name().to_str().unwrap(), "%F %H_%M_%S%.f %z")
+            {
+                v.push((entry, d));
+            }
+        }
+        v.sort_by_key(|(_, d)| *d);
+        let ndel = v.len().saturating_sub(self.backup_count.saturating_sub(1));
+        let mut tasks = Vec::new();
+        for (entry, _) in v.into_iter().take(ndel) {
+            tasks.push(tokio::spawn(async move {
+                fs::remove_dir_all(entry.path())
+                    .await
+                    .expect("Failed to remove directory");
+            }));
+        }
+        let target_backup_folder =
+            backup_world_folder.join(Local::now().format("%F %H_%M_%S%.f %z").to_string());
+        fs::create_dir_all(&target_backup_folder)
+            .await
+            .expect("Failed to create backup directory");
+        let files = self.parse_backup_files(file_paths);
+        for (path, size) in files {
+            let origin_path = self.origin_folder.join(&path);
+            let target_path = target_backup_folder.join(&path);
+            tasks.push(tokio::spawn(async move {
+                fs::create_dir_all(target_path.parent().unwrap())
+                    .await
+                    .expect("Failed to create parent directories");
+                fs::copy(origin_path, &target_path)
+                    .await
+                    .expect("Failed to copy file");
+                fs::OpenOptions::new()
+                    .write(true)
+                    .open(target_path)
+                    .await
+                    .expect("Failed to open file")
+                    .set_len(size)
+                    .await
+                    .expect("Failed to set file size");
+            }));
+        }
+        for task in tasks {
+            task.await.expect("Failed to wait for task");
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,197 @@
-fn main() {
-    println!("Hello, world!");
+mod backup;
+
+use clap::Parser;
+use regex::Regex;
+use std::path::PathBuf;
+use tokio::{
+    fs,
+    io::{self, AsyncBufReadExt, AsyncWriteExt, BufReader, BufWriter},
+    process::Command,
+    time,
+};
+
+const COMMAND_PREFIX: &str = "!";
+
+#[derive(Parser)]
+#[command(version)]
+struct Cli {
+    server_options: Vec<String>,
+
+    #[arg(long = "server-path", default_value = "./bedrock_server", short = 'p')]
+    server_path: PathBuf,
+
+    #[arg(long = "backup-interval", short = 'i')]
+    backup_interval: Option<humantime::Duration>,
+
+    #[arg(long = "backup-folder", default_value = "./backups", short = 'f')]
+    backup_folder: PathBuf,
+
+    #[arg(long = "backup-count", short = 'c')]
+    backup_count: Option<usize>,
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    let cli = Cli::parse();
+    let mut child = Command::new(&cli.server_path)
+        .env("LD_LIBRARY_PATH", cli.server_path.parent().unwrap())
+        .args(cli.server_options)
+        .stdin(std::process::Stdio::piped())
+        .stdout(std::process::Stdio::piped())
+        .current_dir(cli.server_path.parent().unwrap())
+        .spawn()
+        .expect("Failed to start server");
+
+    time::sleep(time::Duration::from_secs(3)).await;
+
+    if !cli.backup_folder.exists() {
+        fs::create_dir_all(&cli.backup_folder)
+            .await
+            .expect("Failed to create backup folder");
+    }
+
+    if !cli.backup_folder.is_dir() {
+        eprintln!("Backup folder must be a directory");
+        std::process::exit(1);
+    }
+
+    let mut self_stdin = BufReader::new(io::stdin()).lines();
+    let mut self_stdout = BufWriter::new(io::stdout());
+    let mut server_stdout = BufReader::new(child.stdout.take().unwrap()).lines();
+    let mut server_stdin = child.stdin.take().unwrap();
+
+    let backuper = backup::Backuper::new(
+        cli.backup_folder,
+        cli.server_path.parent().unwrap().to_path_buf().join("worlds"),
+        cli.backup_count.unwrap_or(1),
+    );
+
+    let mut backup_interval = time::interval(
+        cli.backup_interval
+            .unwrap_or_else(|| humantime::parse_duration("1y").unwrap().into())
+            .into(),
+    );
+    backup_interval.tick().await;
+
+    let command_re = Regex::new(r" *save( +\S*)?").unwrap();
+    let backup_command_re = Regex::new(r"backup *").unwrap();
+    let ready_backups_re =
+        Regex::new(r"\[.+\] Data saved. Files are now ready to be copied.").unwrap();
+
+    loop {
+        tokio::select! {
+            biased;
+            res = child.wait() => {
+                eprintln!("Server exited");
+                res.expect("Failed to wait for server");
+                std::process::exit(0);
+            }
+
+            line = server_stdout.next_line() => {
+                if let Some(line) = line.expect("Failed to read from server stdout") {
+                    self_stdout.write_all(line.as_bytes()).await.expect("Failed to write to stdout");
+                    self_stdout.write_u8(b'\n').await.expect("Failed to write to stdout");
+                    self_stdout.flush().await.unwrap();
+                }
+            }
+
+            line = self_stdin.next_line() => {
+                let line = line
+                    .expect("Failed to read from stdin")
+                    .expect("stdin closed. Don't you redirect stdin?");
+                if line.starts_with(COMMAND_PREFIX) {
+                    if backup_command_re.is_match(line.strip_prefix(COMMAND_PREFIX).unwrap()) {
+                        self_stdout.write_all(b"Backup started\n").await.expect("Failed to write to stdout");
+                        self_stdout.flush().await.expect("Failed to flush stdout");
+                        server_stdin.write_all(b"save hold\n").await.expect("Failed to write to server stdin");
+                        server_stdin.write_all(b"save query\n").await.expect("Failed to write to server stdin");
+                        server_stdin.flush().await.expect("Failed to flush server stdin");
+
+                        server_stdout.next_line().await.expect("Failed to read from server stdout");
+                        loop {
+                            let line = server_stdout.next_line().await
+                                .expect("Failed to read from server stdout")
+                                .expect("Server stdout closed");
+                            if ready_backups_re.is_match(&line) {
+                                break;
+                            }
+                            self_stdout.write_all(line.as_bytes()).await.expect("Failed to write to stdout");
+                            self_stdout.write_u8(b'\n').await.expect("Failed to write to stdout");
+                            self_stdout.flush().await.expect("Failed to flush stdout");
+                            server_stdin.write_all(b"save query\n").await.expect("Failed to write to server stdin");
+                            server_stdin.flush().await.expect("Failed to flush server stdin");
+                            tokio::time::sleep(time::Duration::from_millis(300)).await;
+                        }
+                        let line = server_stdout.next_line().await
+                            .expect("Failed to read from server stdout")
+                            .expect("Server stdout closed");
+                        backuper.backup(line).await;
+                        server_stdin.write_all(b"save resume\n").await.expect("Failed to write to server stdin");
+                        server_stdin.flush().await.expect("Failed to flush server stdin");
+
+                        self_stdout.write_all(b"Backup finished").await.expect("Failed to write to stdout");
+                        self_stdout.flush().await.expect("Failed to flush stdout");
+
+                    } else {
+                        self_stdout.write_all(b"Unknown command").await.expect("Failed to write to stdout");
+                        self_stdout.flush().await.expect("Failed to flush stdout");
+                    }
+                } else if command_re.is_match(&line) {
+                    self_stdout.write_all(b"Don't use \"save xxxx\" command").await.expect("Failed to write to stdout");
+                    self_stdout.flush().await.expect("Failed to flush stdout");
+                } else {
+                    server_stdin.write_all(line.as_bytes()).await.expect("Failed to write to server stdin");
+                    server_stdin.write_u8(b'\n').await.expect("Failed to write to server stdin");
+                    server_stdin.flush().await.expect("Failed to flush server stdin");
+                }
+            }
+
+            _ = backup_interval.tick() => {
+                self_stdout.write_all(b"Backup started\n").await.expect("Failed to write to stdout");
+                self_stdout.flush().await.expect("Failed to flush stdout");
+                server_stdin.write_all(b"save hold\n").await.expect("Failed to write to server stdin");
+                server_stdin.write_all(b"save query\n").await.expect("Failed to write to server stdin");
+                server_stdin.flush().await.expect("Failed to flush server stdin");
+
+                server_stdout.next_line().await.expect("Failed to read from server stdout");
+
+                loop {
+                    let line = server_stdout.next_line().await
+                        .expect("Failed to read from server stdout")
+                        .expect("Server stdout closed");
+                    if ready_backups_re.is_match(&line) {
+                        break;
+                    }
+                    self_stdout.write_all(line.as_bytes()).await.expect("Failed to write to stdout");
+                    self_stdout.write_u8(b'\n').await.expect("Failed to write to stdout");
+                    self_stdout.flush().await.expect("Failed to flush stdout");
+
+                    server_stdin.write_all(b"save query\n").await.expect("Failed to write to server stdin");
+                    server_stdin.flush().await.expect("Failed to flush server stdin");
+                }
+                let line = server_stdout.next_line().await
+                    .expect("Failed to read from server stdout")
+                    .expect("Server stdout closed");
+                backuper.backup(line).await;
+                server_stdin.write_all(b"save resume\n").await.expect("Failed to write to server stdin");
+                server_stdin.flush().await.expect("Failed to flush server stdin");
+
+                self_stdout.write_all(b"Backup finished\n").await.expect("Failed to write to stdout");
+                self_stdout.flush().await.expect("Failed to flush stdout");
+            }
+
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use chrono::{DateTime, Local};
+
+    #[test]
+    fn format_chrono() {
+        let now = Local::now();
+        let formatted = now.format("%F %H_%M_%S%.f %z").to_string();
+        assert_eq!(now, DateTime::parse_from_str(&formatted, "%F %H_%M_%S%.f %z").unwrap());
+    }
 }
